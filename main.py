9df121df3646d09f8a5a3d71baadfb4b3a688a0b
Gxstar <gengxing123@qq.com>
2022-07-12 22:54:34 +0800

    可以正确获得图片

--- main.py
+++ main.py
@@ -10,42 +10,48 @@ header = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/103.0.5060.66 Safari/537.36 Edg/103.0.1264.44',
 }
-o_url = 'https://cn.bing.com/images/search?q='
 
 
 # 获得网页内容
-def get_link(keywords: str, page_num: int):
-    name=urllib.parse.quote(keywords)
+def get_link(keywords: str, num: int):
+    first = 0
+    count = 0
+    name = urllib.parse.quote(keywords)
     if not os.path.exists(f'./image/{keywords}'):
         os.mkdir(f'./image/{keywords}')
-    for i in range(page_num):
-        url = f'https://cn.bing.com/images/search?q={name}&first={i*35+1}&count=35'
-        content = requests.get(url,headers=header).content.decode('utf-8')
-        soup=BeautifulSoup(content,'lxml')
+    while count < num:
+        url = f'https://cn.bing.com/images/async?q={name}&first={first}&count=35&relp=35&lostate=r&mmasync=1'
+        first = first + 35
+        content = requests.get(url, headers=header).content.decode('utf-8')
+        soup = BeautifulSoup(content, 'lxml')
         length = len(soup.select('.iusc'))
-        for item,j in enumerate(soup.select('.iusc')):
-            print(f'当前请求第{i+1}页，第{item+1}项，本页共{length}项，共{page_num}页',end='\r')
-            link=ast.literal_eval(j.attrs['m'])['murl']
-            count=len(os.listdir(f'./image/{keywords}'))+1
-            save_img(link,keywords,count)
+        for item, j in enumerate(soup.select('.iusc')):
+            if count >= num:
+                return False
+            print(f'当前请求第{count + 1}张，共{num}张', end='\r')
+            link = ast.literal_eval(j.attrs['m'])['murl']
+            order = len(os.listdir(f'./image/{keywords}')) + 1
+            save_img(link, keywords, order)
+            count = count + 1
+
 
 # 保存图片
 def save_img(url: str, keywords: str, num: int):
     try:
-        time.sleep(1)
+        time.sleep(0.2)
         r = requests.get(url, stream=True, headers=header)
         if r.status_code == 200:
             open(f'./image/{keywords}/img_{keywords}_{num}.png', 'wb').write(r.content)
         del r
     except Exception:
-        time.sleep(1)
+        time.sleep(0.2)
         print('产生未知错误！')
 
 
 def main():
-    keywords = '彭十六'
-    page_num = 20
-    get_link(keywords, page_num)
+    keywords = '北京'
+    num = 37
+    get_link(keywords, num)
 
 
 # 按间距中的绿色按钮以运行脚本。
